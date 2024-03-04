a8a3269202bf7bed5b2ad2afa37608cbe8fb9e74
Antônio Dias <antoniofdiass@gmail.com>
2024-03-04 20:22:38 -0300

    feat: retrieve data from tmdb's api
    The .env file was created as well.

---
+++ src/services/api.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const tmdbApi = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${process.env.REACT_APP_API_TOKEN}`,
+  },
+});
+
+export const getPopularMovies = async () => {
+  try {
+    const response = await tmdbApi.get('/trending/movie/month?language=en-US');
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching popular movies:', error);
+    throw error;
+  }
+};
