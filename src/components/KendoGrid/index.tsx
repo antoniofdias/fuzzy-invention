352ab9bab3d52943044deac1c82ac5158312aafe
Antônio Dias <antoniofdiass@gmail.com>
2024-03-05 10:20:26 -0300

    feat: create filter input

--- src/components/KendoGrid/index.tsx
+++ src/components/KendoGrid/index.tsx
@@ -6,12 +6,15 @@ import { getImageUrl } from 'utils/image';
 
 export const KendoGrid = () => {
   const [movies, setMovies] = useState([]);
+  const [filteredMovies, setFilteredMovies] = useState([]);
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     const fetchMovies = async () => {
       try {
         const popularMovies = await getPopularMovies();
         setMovies(popularMovies.results);
+        setFilteredMovies(popularMovies.results);
       } catch (error) {
         console.error('Error fetching popular movies:', error);
       }
@@ -20,9 +23,18 @@ export const KendoGrid = () => {
     fetchMovies();
   }, []);
 
+  useEffect(() => {
+    if (movies) {
+      const updatedFilterdMovies = movies?.filter((movie: { title: string }) =>
+        movie.title.toLowerCase().includes(filter.toLowerCase())
+      );
+      setFilteredMovies(updatedFilterdMovies);
+    }
+  }, [filter, movies]);
+
   const gridData: DataResult = {
-    data: movies,
-    total: movies?.length || 0,
+    data: filteredMovies,
+    total: filteredMovies?.length || 0,
   };
 
   const renderImage = (dataItem: {
@@ -38,13 +50,22 @@ export const KendoGrid = () => {
   };
 
   return (
-    movies && (
-      <Grid data={gridData.data} total={gridData.total}>
-        <GridColumn field="poster_path" title="Poster" cell={renderImage} />
-        <GridColumn field="title" title="title" />
-        <GridColumn field="release_date" title="release_date" />
-        <GridColumn field="overview" title="overview" />
-      </Grid>
-    )
+    <>
+      <input
+        type="text"
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+        placeholder="Filter by title"
+        style={{ width: '100%' }}
+      />
+      {movies && (
+        <Grid data={gridData.data} total={gridData.total}>
+          <GridColumn field="poster_path" title="Poster" cell={renderImage} />
+          <GridColumn field="title" title="title" />
+          <GridColumn field="release_date" title="release_date" />
+          <GridColumn field="overview" title="overview" />
+        </Grid>
+      )}
+    </>
   );
 };
